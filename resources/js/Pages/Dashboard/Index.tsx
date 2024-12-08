8370c993a4558ef71d160902086148493dcfb89a
Guntur <goentursumkid@gmail.com>
2024-12-08 16:15:34 +0700

    add dashboard

--- resources/js/Pages/Dashboard/Index.tsx
+++ resources/js/Pages/Dashboard/Index.tsx
@@ -8,20 +8,19 @@ export default function Dashboard() {
             <Head title="Dashboard" />
             <Card>
                 <CardBody>
-
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptatum autem qui quas sint fugit voluptatibus commodi cupiditate nemo culpa hic dicta, adipisci exercitationem reiciendis dolor! Suscipit voluptates distinctio alias.
                 </CardBody>
             </Card>
         </AuthenticatedLayout>
